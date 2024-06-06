86b816135114dc74435dc394a5c63915f2faf1ef
kenji0512 <131452656+kenji0512@users.noreply.github.com>
2024-06-06 20:33:53 +0900

    Add files via upload

---
+++ Level2/MoveSphere.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+
+    //スタートと終わりの目印
+    [SerializeField] Transform startMarker;
+    [SerializeField] Transform endMarker;
+
+    // スピード
+    public float speed = 1.0F;
+
+    //二点間の距離を入れる
+    private float distance_two;
+
+    void Start()
+    {
+        //二点間の距離を代入(スピード調整に使う)
+        distance_two = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+    void Update()
+    {
+
+        // 現在の位置
+        float present_Location = (Time.time * speed) / distance_two;
+
+        // オブジェクトの移動
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, present_Location);
+    }
+}
